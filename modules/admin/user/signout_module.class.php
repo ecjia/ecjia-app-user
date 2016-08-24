aaf5b8c1eaa8a586adfbf8e906a2d6344aef9c7d
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-08-24 17:32:35 +0800

    用户退出登录代码提交

--- modules/admin/user/signout_module.class.php
+++ modules/admin/user/signout_module.class.php
@@ -10,6 +10,7 @@ class signout_module implements ecjia_interface {
 	
 	public function run(ecjia_api & $api) {
 		
+		EM_Api::authSession();
 		RC_Cookie::delete('ECJAP[admin_id]');
 		RC_Cookie::delete('ECJAP[admin_pass]');
 		
--- modules/user/signout_module.class.php
+++ modules/user/signout_module.class.php
@@ -4,10 +4,10 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class signout_module implements ecjia_interface {
 	
 	public function run(ecjia_api & $api) {
+		
+		EM_Api::authSession();
 		RC_Loader::load_app_class('integrate', 'user', false);
-		
 		$user = integrate::init_users();
-		
 		$user->logout();
 		
 		return array();
