48eb20b8b3bd78e4bc2e9f86c8e50137631b293c
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-07 14:37:34 +0800

    调整菜单顺序

--- apis/user_admin_menu_api.class.php
+++ apis/user_admin_menu_api.class.php
@@ -6,7 +6,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class user_admin_menu_api extends Component_Event_Api {	
 	public function call(&$options) {
-		$menus = ecjia_admin::make_admin_menu('08_members', __('会员管理'), '', 6);
+		$menus = ecjia_admin::make_admin_menu('08_members', __('会员管理'), '', 8);
 		$submenus = array(
 				ecjia_admin::make_admin_menu('03_users_list', __('会员列表'), RC_Uri::url('user/admin/init'), 1)->add_purview('users_manage'),
 				ecjia_admin::make_admin_menu('04_users_add', __('添加会员'), RC_Uri::url('user/admin/add'), 2)->add_purview('users_manage'),
