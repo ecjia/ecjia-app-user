4c28e32f9d818b3edbeab89b55c8b8a1033ae72c
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 20:36:13 +0800

    app下model修改

--- model/shipping_viewmodel.class.php
+++ model/shipping_viewmodel.class.php
@@ -6,8 +6,6 @@ class shipping_viewmodel extends Component_Model_View {
 	public $view = array();
 	
 	public function __construct() {
-		$this->db_config = RC_Config::load_config('database');
-		$this->db_setting = 'default';
 		$this->table_name = 'shipping';
 		$this->table_alias_name	= 's';
 		
