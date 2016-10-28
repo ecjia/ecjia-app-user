171cafd4df0771f5f8cd16827beca3429390657c
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-28 10:07:16 +0800

    推荐分成，第三方登录等新接口首次迁移提交

---
+++ modules/user/bonus_module.class.php
@@ -0,0 +1,25 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+/**
+ * 会员红包列表
+ * @author will.chen
+ *
+ */
+class bonus_module extends api_front implements api_interface
+{
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request)
+    {
+		$this->authSession();
+		if ($_SESSION['user_id'] <= 0) {
+			return new ecjia_error(100, 'Invalid session');
+		}
+		
+		$bonus_type		= $this->requestData('bonus_type');
+		$bonus_list = RC_Api::api('user', 'user_bonus_list', array('bonus_type' => $bonus_type));
+		
+		return $bonus_list;
+	}
+}
+
+
+// end
