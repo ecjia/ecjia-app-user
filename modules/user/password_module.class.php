0eb6a3075e221df49086a646acdae93f18ef98a2
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-01 17:53:00 +0800

    分页样式调用修改

--- modules/user/password_module.class.php
+++ modules/user/password_module.class.php
@@ -8,10 +8,12 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class password_module extends api_front implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
     	
-    	if ($_SESSION['user_id'] <= 0) {
-			return new ecjia_error(100, 'Invalid session');
-		}
-    	$user_id = $_SESSION['user_id'];
+        //如果用户登录获取其session
+        $this->authSession();
+        $user_id = $_SESSION['user_id'];
+    	if ($user_id <= 0) {
+    		return new ecjia_error(100, 'Invalid session');
+    	}
     	
  		RC_Loader::load_app_class('integrate', 'user', false);
  		$user = integrate::init_users();
