a4abc8aacd52a10dd21194b160d77163e467b4dc
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-07 18:38:30 +0800

    会员头像更新方式修改提交

--- modules/user/update_module.class.php
+++ modules/user/update_module.class.php
@@ -18,35 +18,50 @@ class update_module extends api_front implements api_interface {
 		$db = RC_Model::model('user/users_model');
 		
 		if (!empty($img)) {
-			$uid = $user_id;
-			$userinfo = $db->field('user_name')->find(array('user_id' => $uid));
+			
+			$save_path = 'data/avatar_img';
+			$upload = RC_Upload::uploader('image', array('save_path' => $save_path, 'auto_sub_dirs' => true));
 				
-			$uid = abs(intval($uid));//保证uid为绝对的正整数
-				
-			$uid = sprintf("%09d", $uid);//格式化uid字串， d 表示把uid格式为9位数的整数，位数不够的填0
-				
-			$dir1 = substr($uid, 0, 3);//把uid分段
-			$dir2 = substr($uid, 3, 2);
-			$dir3 = substr($uid, 5, 2);
-				
-			if (empty($userinfo)) {
-				return new ecjia_error('user_error', __('用户信息错误！'));
+			$image_info	= $upload->upload($_FILES['avatar_img']);
+			/* 判断是否上传成功 */
+			if (!empty($image_info)) {
+				$avatar_img = $upload->get_position($image_info);
+				$db->where(array('user_id' => $_SESSION['user_id']))->update(array('avatar_img' => $avatar_img));
+			} else {
+				return new ecjia_error('avatar_img_error', '头像上传失败！');
 			}
+			
+			
+			
+// 			$uid = $user_id;
+// 			$userinfo = $db->field('user_name')->find(array('user_id' => $uid));
 				
-			$filename = md5($userinfo['user_name']);
-			
-			$path = RC_Upload::upload_path() . 'data' . DIRECTORY_SEPARATOR . 'avatar' . DIRECTORY_SEPARATOR . $dir1 . DIRECTORY_SEPARATOR . $dir2 . DIRECTORY_SEPARATOR . $dir3;
-			$filename_path = $path. DIRECTORY_SEPARATOR . substr($uid, -2)."_".$filename.'.jpg';
+// 			$uid = abs(intval($uid));//保证uid为绝对的正整数
 				
-			//创建目录
-			$result = RC_Filesystem::mkdir($path, 0777, true, true);
+// 			$uid = sprintf("%09d", $uid);//格式化uid字串， d 表示把uid格式为9位数的整数，位数不够的填0
+				
+// 			$dir1 = substr($uid, 0, 3);//把uid分段
+// 			$dir2 = substr($uid, 3, 2);
+// 			$dir3 = substr($uid, 5, 2);
+				
+// 			if (empty($userinfo)) {
+// 				return new ecjia_error('user_error', __('用户信息错误！'));
+// 			}
+				
+// 			$filename = md5($userinfo['user_name']);
 			
-			//删除原有图片
-			RC_Filesystem::delete($filename_path);
+// 			$path = RC_Upload::upload_path() . 'data' . DIRECTORY_SEPARATOR . 'avatar' . DIRECTORY_SEPARATOR . $dir1 . DIRECTORY_SEPARATOR . $dir2 . DIRECTORY_SEPARATOR . $dir3;
+// 			$filename_path = $path. DIRECTORY_SEPARATOR . substr($uid, -2)."_".$filename.'.jpg';
+				
+// 			//创建目录
+// 			$result = RC_Filesystem::mkdir($path, 0777, true, true);
 			
-			@unlink($filename_path);//删除原有图片
-			$img = base64_decode($img);
-			file_put_contents($filename_path, $img);//返回的是字节数printr(a);
+// 			//删除原有图片
+// 			RC_Filesystem::delete($filename_path);
+			
+// 			@unlink($filename_path);//删除原有图片
+// 			$img = base64_decode($img);
+// 			file_put_contents($filename_path, $img);//返回的是字节数printr(a);
 		}
 		
 		if (!empty($user_name)) {
