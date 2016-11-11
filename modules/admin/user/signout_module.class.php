ecd4dd06dfcff4efa1c8df36863db00a68cf6f66
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-11 19:32:30 +0800

    后台退出登录修改

--- modules/admin/user/signout_module.class.php
+++ modules/admin/user/signout_module.class.php
@@ -8,10 +8,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class signout_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    		
-		$this->authadminSession();
-		RC_Cookie::delete('ECJAP[admin_id]');
-		RC_Cookie::delete('ECJAP[admin_pass]');
+    	
 		
 		RC_Session::destroy();
 		
