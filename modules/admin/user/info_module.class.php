1c6f029fc21953cbfe9ae22f9615aa8e6090ef00
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-20 16:01:09 +0800

    更新user/info

--- modules/admin/user/info_module.class.php
+++ modules/admin/user/info_module.class.php
@@ -19,30 +19,10 @@ class info_module extends api_admin implements api_interface {
 			return new ecjia_error(101, '错误的参数提交');
 		}
 		
-		$user_info = RC_Api::api('user', 'user_info', array('user_id' => $user_id, 'mobile' => $mobile));
+		RC_Loader::load_app_func('admin_user', 'user');
+		$user_info = EM_user_info($user_id);
 		
-		if (is_ecjia_error($user_info)) {
-			return $user_info;
-		}
-		
-		/* 获取可使用的红包数量*/
-		$bonus_count = RC_Model::model('bonus/user_bonus_type_viewmodel')->join('bonus_type')->where(array('ub.user_id' => $user_id, 'use_start_date' => array('lt' => RC_Time::gmtime()), 'use_end_date' => array('gt' => RC_Time::gmtime()), 'ub.order_id' => 0))->count("*");
-		
-		return array(
-			'id'			=> $user_info['user_id'],
-			'name'			=> $user_info['user_name'],
-			'rank_name' 	=> $user_info['user_rank_name'],
-			'email'			=> $user_info['email'],
-			'mobile_phone'	=> $user_info['mobile_phone'],
-			'user_money'	=> $user_info['user_money'],
-			'formatted_user_money'	=> $user_info['formated_user_money'],
-			'user_points'			=> $user_info['pay_points'],
-			'user_bonus_count'		=> $bonus_count,
-			'reg_time'		=> RC_Time::local_date(ecjia::config('time_format'), $user_info['reg_time']),
-			'last_login'	=> RC_Time::local_date(ecjia::config('time_format'), $user_info['last_login']),
-			'address'		=> $user_info['address'],
-			'avatar_img'	=> $user_info['avatar_img'],
-		);
+		return $user_info;
 	}
 }
 
