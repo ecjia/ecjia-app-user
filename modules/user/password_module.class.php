b14a58a92aea68d0c0efd93f23b029e5a1b43985
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-08 09:59:28 +0800

    修改密码接口修复

--- modules/user/password_module.class.php
+++ modules/user/password_module.class.php
@@ -69,8 +69,10 @@ class password_module extends api_front implements api_interface {
 		$user_id	  = $this->requestData('uid', $_SESSION['user_id']);
 	    
 		if (strlen($new_password) < 6) {
-	    	$result = new ecjia_error('password_shorter', __('- 登录密码不能少于 6 个字符。'));
-	    	return $result;
+	    	return new ecjia_error('password_shorter', __('- 登录密码不能少于 6 个字符。'));
+	    }
+	    if ($old_password == $new_password) {
+	        return new ecjia_error('password_shorter', '新密码不能和原密码相同！');
 	    }
 	
 	    $user_info = $user->get_profile_by_id($user_id); //论坛记录
