b3b04d5637f9cdefaa350cf1ad036143d2cb39d1
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-10-24 16:15:15 +0800

    更新表名

--- model/region_model.class.php
+++ model/region_model.class.php
@@ -49,7 +49,7 @@ defined ( 'IN_ECJIA' ) or exit ( 'No permission resources.' );
 class region_model extends Component_Model_Model {
 	public $table_name = '';
 	public function __construct() {
-		$this->table_name = 'region';
+		$this->table_name = 'regions';
 		parent::__construct ();
 	}
 	
