413caa230cde88044aaa7c98140350f3075bc0d5
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-12-04 11:40:53 +0800

    修改编辑收货地址

--- modules/address/update_module.class.php
+++ modules/address/update_module.class.php
@@ -72,9 +72,10 @@ class update_module extends api_front implements api_interface {
 		$address['consignee']     = isset($address['consignee']) ? trim($address['consignee']) : '';
 		
 // 		$address['country']       = isset($address['country']) ? trim($address['country']) : '';
-// 		$address['province']      = isset($address['province']) ? trim($address['province']) : '';
+		$address['province']      = isset($address['province']) ? trim($address['province']) : '';
 		$address['city']      	  = isset($address['city']) ? trim($address['city']) : '';
 		$address['district']      = isset($address['district']) ? trim($address['district']) : '';
+		$address['street']        = isset($address['street']) ? trim($address['street']) : '';
 
 		$address['email']         = !empty($address['email']) ? trim($address['email']) : '';
 		$address['mobile']        = isset($address['mobile']) ? trim($address['mobile']) : '';
@@ -89,7 +90,7 @@ class update_module extends api_front implements api_interface {
 			$address['address'] = trim($address['wx_address']);
 		}
 		
-		$data = ecjia_region::getSplitRegionWithKey($address['city']);
+		$data = ecjia_region::getSplitRegionWithKey($address['street']);
 
 		$address['country']		= $data['country'];
 		$address['province']	= $data['province'];
