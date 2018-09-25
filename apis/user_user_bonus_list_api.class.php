22e984cffe01cc36500cb0fd838892b48c0ff6e5
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-25 09:37:33 +0800

    user_bonus_list_api调用参数默认值

--- apis/user_user_bonus_list_api.class.php
+++ apis/user_user_bonus_list_api.class.php
@@ -73,7 +73,10 @@ class user_user_bonus_list_api extends Component_Event_Api {
     	
     	$count = $db->count(RC_DB::raw('ub.bonus_id'));
     	
-    	$page_row = new ecjia_page($count, $options['size'], 6, '', $options['page']);
+    	$size = isset($options['size']) ? 15 : $options['size'];
+    	$page = isset($options['page']) ? 1 : $options['page'];
+    	
+    	$page_row = new ecjia_page($count, $size, 6, '', $page);
     	$filed = 'ub.bonus_id, ub.order_id, bt.type_name, bt.type_money, bt.min_goods_amount, bt.use_start_date, bt.use_end_date, s.store_id, s.manage_mode, s.merchants_name';
     	
     	$rows = $db->select(RC_DB::raw($filed))
