dffaaa7421e3d27866cc5b755d3a00a118d03c98
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-19 11:50:50 +0800

    封装ecjia_integrate类

---
+++ classes/Integrate/UserIntegrateServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ecjia\App\User\Integrate;
+
+use Ecjia\App\User\Integrate\IntegratePlugin;
+use Ecjia\App\User\Integrate\UserManager;
+use Royalcms\Component\Support\ServiceProvider;
+
+class UserIntegrateServiceProvider extends  ServiceProvider
+{
+    
+
+
+    public function register()
+    {
+        $this->registerIntegrateManager();
+
+        $this->registerIntegratePlugin();
+
+
+        $this->loadAlias();
+    }
+
+
+    protected function registerIntegrateManager()
+    {
+        $this->royalcms->bindShared('ecjia.integrate', function($royalcms) {
+            return new UserManager();
+        });
+    }
+
+
+    protected function registerIntegratePlugin()
+    {
+        $this->royalcms->bindShared('ecjia.integrate.plugin', function($royalcms) {
+            return new IntegratePlugin();
+        });
+    }
+
+
+    /**
+     * Load the alias = One less install step for the user
+     */
+    protected function loadAlias()
+    {
+        $this->royalcms->booting(function()
+        {
+            $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
+            $loader->alias('ecjia_integrate', 'Ecjia\App\User\Integrate\Facades\EcjiaIntegrate');
+
+        });
+    }
+    
+}
